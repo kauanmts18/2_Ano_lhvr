40bc45431a8e9b9963cbebced17d6696f19799f9
Ericson Costa <costaericson32@gmail.com>
2024-06-05 14:25:03 -0300

    fim de aula

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,23 @@
+const users = [];
+
 class User {
   index(req, res) {
-    res.render("home");
+    res.render("home", { users });
+  }
+  store(req, res) {
+    console.log(req.body);
+    const { nome, email } = req.body; //desestruturando e criando as variaveis nome e email
+    users.push({ nome, email }); //adicionando as informacoes no array
+    res.redirect("/");
+  }
+  removeStore(req, res) {
+    const { nome, email } = req.body;
+    let index = users.findIndex(
+      (user) => user.nome == nome && user.email == email
+    );
+    users.splice(index, 1);
+    console.log(index);
+    res.redirect("/");
   }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require("../controller/userController");
 
 router.get("/", userController.index);
+router.post("/", userController.store);
+router.post("/remover", userController.removeStore);
 
 module.exports = router;
