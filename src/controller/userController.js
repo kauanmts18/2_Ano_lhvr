0548b6574cbb3c7b0e0d2b935c6e684efaa5abf3
Ericson Costa <costaericson32@gmail.com>
2024-06-06 11:26:19 -0300

    aula06-06-2024

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -5,7 +5,7 @@ class User {
     res.render("home", { users });
   }
   store(req, res) {
-    console.log(req.body);
+    console.log(req.body); //estou mostrando no console o que vem do usuario
     const { nome, email } = req.body; //desestruturando e criando as variaveis nome e email
     users.push({ nome, email }); //adicionando as informacoes no array
     res.redirect("/");
@@ -13,13 +13,27 @@ class User {
   removeStore(req, res) {
     const { nome, email } = req.body;
     let index = users.findIndex(
-      (user) => user.nome == nome && user.email == email
+      (user) => user.nome === nome && user.email === email
     );
-    users.splice(index, 1);
-    console.log(index);
+    if (index === -1) {
+      //se nao tiver usuario no array
+      return res.status(404).send("Usuario nao encontrado");
+    }
+    users.splice(index, 1); //remove um o elemento do array
     res.redirect("/");
+    // console.log(index);
   }
 }
 
 const user = new User();
 module.exports = user;
+
+// removeStore(req, res) {
+//   const { nome, email } = req.body;
+//   let index = users.findIndex(
+//     (user) => user.nome == nome && user.email == email
+//   );
+//   users.splice(index, 1);
+//   console.log(index);
+//   res.redirect("/");
+// }
